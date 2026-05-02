fdd01dc0790e50b36f04af54acf23d06c259ba1f
illya <illyaklus2@gmail.com>
2026-05-03 02:16:44 +0300

    npm

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 import { AuthRouter } from "./modules/auth/auth.router.js";
 import { AuthMiddleware } from "./middlewares/auth/auth.middleware.js";
 dotenv.config();
@@ -9,21 +9,34 @@ import { ProductsRouter } from "./modules/products/products.router.js";
 import { CartRouter } from "./modules/cart/cart.router.js";
 import { CartItemRouter } from "./modules/cartItem/cartItem.router.js";
 import { OrderRouter } from "./modules/order/order.router.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+const swaggerDocument = YAML.load("./swagger.yaml");
 
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5000"
+] 
 
 
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+        return;
+      }
+      callback(new Error("Not allowed by CORS"));
+    } ,
     credentials: true, 
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
-
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/auth", AuthRouter);
 app.use("/products", ProductsRouter);
 app.use("/cart", AuthMiddleware, CartRouter);
