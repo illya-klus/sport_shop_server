e3a27b97ba4c69c75ebc1468339233fd9aa829c4
illya <illyaklus2@gmail.com>
2026-05-03 03:12:33 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const swaggerDocument = YAML.load("./swagger.yaml");
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5000",
-  "https://sportua.vercel.app/"
+  "https://sportua.vercel.app"
 ]
 
 
